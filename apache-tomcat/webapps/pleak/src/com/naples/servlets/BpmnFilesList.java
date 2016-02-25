7e7c939d920d579101c99d4917a1a150cf44cfed
Marko Jõemets <marko.joemets@cyber.ee>
2016-02-25 19:03:51 +0200

    Now files list returns files list from the right folder.

--- apache-tomcat/webapps/pleak/src/com/naples/servlets/BpmnFilesList.java
+++ apache-tomcat/webapps/pleak/src/com/naples/servlets/BpmnFilesList.java
@@ -1,8 +1,7 @@
 package com.naples.servlets;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.nio.file.*;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,9 +12,26 @@ public class BpmnFilesList extends HttpServlet{
  @Override
  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
                   throws ServletException, IOException {
+
+     String pathStr = getServletContext().getRealPath(getServletContext().getInitParameter("bpmn-files-dir"));
+     Path filesPath = Paths.get(pathStr);
+
+     String files = "[";
+     try (DirectoryStream<Path> stream = Files.newDirectoryStream(filesPath, "*.bpmn")) {
+         for (Path entry: stream) {
+             files = files + "'" + entry.getFileName() + "', ";
+         }
+     } catch (DirectoryIteratorException ex) {
+         throw ex.getCause();
+     }
+     if (!files.equals("[")) {
+         files = files.substring(0, files.length() - 2);
+     }
+     files = files + "]";
+
      resp.setContentType("application/json");
      resp.setCharacterEncoding("UTF-8");
-     resp.getWriter().write("{files: []}");
+     resp.getWriter().write(files);
 
  }
 
