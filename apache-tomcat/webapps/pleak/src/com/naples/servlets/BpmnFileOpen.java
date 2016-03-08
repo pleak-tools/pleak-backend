22d918faa81222cfd2d5ba37a1ab61f650896f18
Henry Mägi <henry.magi@cyber.ee>
2016-03-08 10:13:11 +0200

    Refactored file opening

--- apache-tomcat/webapps/pleak/src/com/naples/servlets/BpmnFileOpen.java
+++ apache-tomcat/webapps/pleak/src/com/naples/servlets/BpmnFileOpen.java
@@ -8,28 +8,43 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.naples.responses.JsonResponse;
+import com.naples.helpers.FileException;
+import com.naples.helpers.FileHelper;
+
 public class BpmnFileOpen extends HttpServlet{
 
  @Override
  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
                   throws ServletException, IOException {
 
+    String result = "";
+
+    JsonResponse jsonResponse = new JsonResponse();
+    FileHelper fh = new FileHelper();
+
     String fileName = req.getParameter("fileName");
 
-    String filePathStr = getServletContext().getRealPath(getServletContext().getInitParameter("bpmn-files-dir")) + fileName;
-    Path filePath = Paths.get(filePathStr);
-
-    String fileContents = "";
     try {
-      if (Files.isReadable(filePath)) {
-        fileContents = new String(Files.readAllBytes(filePath));
-      }
-    }
-    catch (Exception e) {}
 
-  resp.setContentType("application/json");
-  resp.setCharacterEncoding("UTF-8");
-  resp.getWriter().write(fileContents);
+      if ( !fh.isCorrectFileName(fileName) ) throw new FileException("Incorrect file name.");
+      if ( !fh.isCorrectFileExtension(fileName) ) throw new FileException("Incorrect file extension.");
+
+      String filePathStr = getServletContext().getRealPath(getServletContext().getInitParameter("bpmn-files-dir")) + fileName;
+      Path filePath = Paths.get(filePathStr);
+
+      if (Files.isReadable(filePath)) {
+        result = new String(Files.readAllBytes(filePath));
+      }
+
+    }
+    catch (Exception e) {
+      result = jsonResponse.getJsonResponseError(400, e.getMessage());
+    }
+
+    resp.setContentType("application/json");
+    resp.setCharacterEncoding("UTF-8");
+    resp.getWriter().write(result);
 
  }
 
